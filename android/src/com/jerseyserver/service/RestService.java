0ed746d69f395eb9635e4ae388b5532eafde7b89
xietengxiao <775692117@qq.com>
2017-06-25 16:54:15 +0800

    服务器最终版

--- android/src/com/jerseyserver/service/RestService.java
+++ android/src/com/jerseyserver/service/RestService.java
@@ -1,6 +1,7 @@
 package com.jerseyserver.service;
 
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import com.jerseyserver.entity.*;
 import javax.ws.rs.FormParam;
@@ -71,6 +72,190 @@ public class RestService {
         }  	    
 		System.out.println(log);
 		return log;
+	}
+	
+	ArrayList<Integer> checkseat(int movie, int row) {
+		ArrayList<Integer> arraylist= new ArrayList<Integer>();
+		boolean check = false;
+		String sql="";
+		String sql1 = "";
+		try {   
+            Class.forName(driver);   
+            Connection conn = DriverManager.getConnection(url, user, password); 
+            Connection conn1 = DriverManager.getConnection(url, user, password); 
+            if (!conn.isClosed()) {   
+                System.out.println("Succeeded connecting to the Cinemabase!");
+                String table = "";
+                switch(movie) {
+                case 1:
+                	table = "cinema1";
+                	sql = "select * from cinema1 where id = ? ";
+                	sql1="update cinema1 set ";  
+                	break;
+                case 2:
+                	table = "cinema2";
+                	sql = "select * from cinema2 where id = ? ";
+                	sql1="update cinema2 set "; 
+                	break;
+                case 3:
+                	table = "cinema3";
+                	sql = "select * from cinema3 where id = ? ";
+                	sql1="update cinema3 set "; 
+                	break;
+                case 4:
+                	table = "cinema4";
+                	sql = "select * from cinema4 where id = ? ";
+                	sql1="update cinema4 set "; 
+                	break;
+                case 5:
+                	table = "cinema5";
+                	sql = "select * from cinema5 where id = ? ";
+                	sql1="update cinema5 set "; 
+                	break;
+                case 6:
+                	table = "cinema6";
+                	sql = "select * from cinema6 where id = ? ";
+                	sql1="update cinema6 set "; 
+                	break;
+                default:
+                	break;
+                }
+                
+                PreparedStatement ps=conn.prepareStatement(sql);
+                ps.setInt(1, row);
+                ResultSet rs = ps.executeQuery(); 
+                while(rs.next()){
+                	for (int i= 2;i<=15;i++ )
+                	if (rs.getInt(i) == 1)
+                	  arraylist.add(i-1);
+                }
+                
+                conn.close();
+                conn1.close();
+            }
+        } catch (Exception e) {   
+            e.printStackTrace();   
+        }  	    
+		System.out.println(check);
+		return arraylist;
+	}
+	
+	boolean bookseat(int movie, int row, int column) {
+		boolean book = false;
+		String sql="";
+		String sql1 = "";
+		try {   
+            Class.forName(driver);   
+            Connection conn = DriverManager.getConnection(url, user, password); 
+            Connection conn1 = DriverManager.getConnection(url, user, password); 
+            if (!conn.isClosed()) {   
+                System.out.println("Succeeded connecting to the Cinemabase!");
+                String table = "";
+                switch(movie) {
+                case 1:
+                	table = "cinema1";
+                	sql = "select * from cinema1 where id = ? ";
+                	sql1="update cinema1 set ";  
+                	break;
+                case 2:
+                	table = "cinema2";
+                	sql = "select * from cinema2 where id = ? ";
+                	sql1="update cinema2 set "; 
+                	break;
+                case 3:
+                	table = "cinema3";
+                	sql = "select * from cinema3 where id = ? ";
+                	sql1="update cinema3 set "; 
+                	break;
+                case 4:
+                	table = "cinema4";
+                	sql = "select * from cinema4 where id = ? ";
+                	sql1="update cinema4 set "; 
+                	break;
+                case 5:
+                	table = "cinema5";
+                	sql = "select * from cinema5 where id = ? ";
+                	sql1="update cinema5 set "; 
+                	break;
+                case 6:
+                	table = "cinema6";
+                	sql = "select * from cinema6 where id = ? ";
+                	sql1="update cinema6 set "; 
+                	break;
+                default:
+                	break;
+                }
+                
+                PreparedStatement ps=conn.prepareStatement(sql);
+                ps.setInt(1, row);
+                ResultSet rs = ps.executeQuery();    
+               while(rs.next()){
+            	   System.out.println(sql);
+            	   System.out.println(rs.getInt(column+1));
+            	   if (rs.getInt(column+1) == 0){
+            		   String c = "";
+            		   switch(column) {
+            		   case 1:
+            			   c = "c1 = 1 where id = ?";
+            			   break;
+            		   case 2:
+            			   c = "c2 = 1 where id = ?";
+            			   break;
+            		   case 3:
+            			   c = "c3 = 1 where id = ?";
+            			   break;
+            		   case 4:
+            			   c = "c4 = 1 where id = ?";
+            			   break;
+            		   case 5:
+            			   c = "c5 = 1 where id = ?";
+            			   break;
+            		   case 6:
+            			   c = "c6 = 1 where id = ?";
+            			   break;
+            		   case 7:
+            			   c = "c7 = 1 where id = ?";
+            			   break;
+            		   case 8:
+            			   c = "c8  = 1 where id = ?";
+            			   break;
+            		   case 9:
+            			   c = "c9  = 1 where id = ?";
+            			   break;
+            		   case 10:
+            			   c = "c10 = 1 where id = ?";
+            			   break;
+            		   case 11:
+            			   c = "c11 = 1 where id = ?";
+            			   break;
+            		   case 12:
+            			   c = "c12 = 1 where id = ?";
+            			   break;
+            		   case 13:
+            			   c = "c13 = 1 where id = ?";
+            			   break;
+            		   case 14:
+            			   c = "c14 = 1 where id = ?";
+            			   break;
+            			   default:
+            				   break;
+            		   }
+            		   String sql2 = sql1 + c;
+            		   System.out.println(sql2);
+                   PreparedStatement ps1=conn1.prepareStatement(sql2);
+                   ps1.setInt(1, row);
+                   int result=ps1.executeUpdate();
+                       conn.close();
+                       conn1.close();
+            		   book = true; 
+            	   }
+               }
+                conn.close();
+            }   
+        } catch (Exception e) {   
+            e.printStackTrace();   
+        }  	    
+		return book;
 	}
 	
 	@GET
@@ -177,8 +362,82 @@ ObjectMapper mapper = new ObjectMapper();
         	return null;
 
 }
+	private ArrayList<Integer> checkallseat(String movie, String count){
+		ArrayList<Integer> arraylist= new ArrayList<Integer>();
+		for (int i= 1; i<=10;i++){
+			ArrayList<Integer> temp=checkseat(Integer.parseInt(movie),Integer.parseInt(count+i));
+			for (int j= 0;j< temp.size();j++){
+				arraylist.add(i);
+				arraylist.add(temp.get(j));
+				
+			}
+					
+				}
+			
+		
+			return arraylist;
+		}
+	
+	@POST
+	@Path("/book")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response book(@FormParam("movie") String movie, @FormParam("row")String row, @FormParam("column")String column) {
+		ObjectMapper mapper = new ObjectMapper();
+		System.out.println(row);
+		System.out.println(column);
+		if(bookseat(Integer.parseInt(movie), Integer.parseInt(row), Integer.parseInt(column))){
+		   System.out.println("here!"); 
+		        GenericEntity<String> payloadEntity;
+		        try {
+		            payloadEntity = new GenericEntity<String>(
+		                    mapper.writeValueAsString(new ResponseDTO(200, "ok", user))) {
+		            };
+		            return Response.ok(payloadEntity).build();
+		        } catch (JsonProcessingException e) {
+		            e.printStackTrace();
+		            return Response
+		                    .ok(" {\"status\": 404,\n\"message\": \"error\",\n\"response\": \"\"}")
+		                    .build();	 
+			}
+		        }
+		        else
+		        	return null;
+
+		}
+
+	
+	@POST
+	@Path("/check")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response check(@FormParam("movie") String movie, @FormParam("count")String count) {
+		ObjectMapper mapper = new ObjectMapper();
+		ArrayList<Integer> arrayList=checkallseat(movie, count);
+		String temp="";
+		for (int i= 0;i< arrayList.size();i++){
+			if (i!= 0)
+			temp+=String.valueOf(","+arrayList.get(i)+",");
+			else
+				temp+=String.valueOf(arrayList.get(i)+",");
+			temp+=String.valueOf(arrayList.get(++i));
+		}
+		   System.out.println(temp); 
+		        GenericEntity<String> payloadEntity;
+		        try {
+		            payloadEntity = new GenericEntity<String>(
+		                    mapper.writeValueAsString(new ResponseDTO(200, temp, user))) {
+		            };
+		            return Response.ok(payloadEntity).build();
+		        } catch (JsonProcessingException e) {
+		            e.printStackTrace();
+		            return Response
+		                    .ok(" {\"status\": 404,\n\"message\": \"error\",\n\"response\": \"\"}")
+		                    .build();	 
+			}
+		        
+
+		}
+	
 }
 	
 		
-	
 	
